0e777762757420bbc6ea33ec471a089b0c9e1f67
Wang Zhijun <so@idcf.jp>
2016-01-29 14:29:57 +0900

    prepare.shを修正

--- scripts/ci/prepare.sh
+++ scripts/ci/prepare.sh
@@ -1,4 +1,4 @@
-#!/user/bin/env bash
+#!/usr/bin/env bash
 
 sudo wget https://raw.githubusercontent.com/rabbitmq/rabbitmq-management/rabbitmq_v3_6_0/bin/rabbitmqadmin -O /usr/bin/rabbitmqadmin
 sudo chmod 0755 /usr/bin/rabbitmqadmin
