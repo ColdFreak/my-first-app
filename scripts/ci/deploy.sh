5b6d3ecb8eb9aeb4b4fe338c34c509e3e268e8ce
Wang Zhijun <so@idcf.jp>
2016-01-29 15:25:52 +0900

    scripts/ci/deploy.sh

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 #rabbitmqadmin -H 210.140.87.185 -P 12345 --ssl -u deploy -p deploy publish exchange=rel-exchange routing_key=rel-queue payload="greeting from circle ci second time"
-$SEND_DEPLOY_MESSAGE
+send_deploy_message=$SEND_DEPLOY_MESSAGE
+send_deploy_message
