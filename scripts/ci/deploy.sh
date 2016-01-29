d9e6ef8555705cefdaacb0ef44afed58238d1c9b
Wang Zhijun <so@idcf.jp>
2016-01-29 16:34:43 +0900

    deploy.sh修正

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+commit_id=$(git log --format="%H" -n 1)
 datetime=$(date)
-rabbitmqadmin -H 210.140.87.185 -P 12345 --ssl -u deploy -p deploy publish exchange=rel-exchange routing_key=rel-queue payload="{\"branch\": \"develop\", \"datetime\": \"$datetime\"}"
+rabbitmqadmin -H 210.140.87.185 -P 12345 --ssl -u deploy -p deploy publish exchange=rel-exchange routing_key=rel-queue payload="{\"branch\": \"develop\", \"datetime\": \"$datetime\", \"commit_id\": \"$commit_id\"}"
